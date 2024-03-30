4ce5d82d7b7fa80e708b2c2ad6f7bc01c03f43aa
Kiarash Hesampour <kiarash.hesampour.624@my.csun.edu>
2024-03-29 20:52:12 -0700

    march 29 is not fixed we have huge problems

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Country } from './country';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 
 @Component({
   selector: 'app-countries',
@@ -8,5 +11,23 @@ import { Component } from '@angular/core';
   styleUrl: './countries.component.css'
 })
 export class CountriesComponent {
+  public forecasts: Country[] = [];
+  countries: Country[] | undefined;
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.getCountries();
+
+  }
+
+  getCountries() {
+    this.http.get<Country[]>(environment.baseUrl+'api/Countries').subscribe(
+      {
+       next : result => this.countries ,
+       error : (error: any) => console.error(error)
+      }
+
+      
+    );  
+  }
 }
